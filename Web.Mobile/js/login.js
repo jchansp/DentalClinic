46f0551b33aec3dfd8c88bcda4b9b19cc9fd691d
jchansp <jciansp@hotmail.com>
2015-06-17 16:16:02 +0200

    Parse.com integration

--- Web.Mobile/js/login.js
+++ Web.Mobile/js/login.js
@@ -1,19 +1,28 @@
-﻿var uri = "http://localhost:1777/api/";
+﻿var applicationId = 'C7xD9l2BrBM4y8n4H2kMSHlipUTkKu4v5JeoWnLG';
+var restApiKey = 'epEUYTX3LBpapTgTXF7xcBLZHB5wIaUCcZj0QMcp';
 $$(".login-screen").find(".list-button").on("click", function() {
     myApp.showPreloader();
     var username = $$(".login-screen").find("input[name=\"username\"]").val();
     var password = $$(".login-screen").find("input[name=\"password\"]").val();
-    myApp.getJSON(uri + "/" + id)
-        .done(function(data) {
-            $("#product").text(formatItem(data));
-        })
-        .fail(function(jqXHR, textStatus, err) {
-            $("#product").text("Error: " + err);
-        });
-    //setTimeout(function() {
-    //    myApp.hidePreloader();
-    //    myApp.alert("Username: " + username + ", password: " + password, function() {
-    //        myApp.closeModal(".login-screen");
-    //    });
-    //}, 500);
+    var query = 'https://api.parse.com/1/login?username=' + username + '&password=' + password;
+    $$.ajax({
+        url: query,
+        headers: { "X-Parse-Application-Id": applicationId, "X-Parse-REST-API-Key": restApiKey },
+        type: "GET",
+        success: function (data, textStatus) {
+            myApp.hidePreloader();
+            data = JSON.parse(data);
+            if (!data.username) { return }
+            var username = data.username;
+            myApp.alert("Username: " + username + ", password: " + password, function() {
+                myApp.closeModal(".login-screen");
+            });
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            myApp.hidePreloader();
+            myApp.alert('Login was unsuccessful, please verify username and password and try again');
+            $$('#login-email').val('');
+            $$('#login-password').val('');
+        }
+    });
 });
